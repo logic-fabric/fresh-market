60aa02c372351669d63f40accfbac6c5e533a18f
logic-fabric <lowic@logic-fabric.fr>
2021-07-29 10:35:26 +0200

    feature: add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Header.css";
+
+export function Header() {
+  return (
+    <header className="bg-gradient-to-r from-primary-600 to-primary-300 ">
+      <div className="content-wrapper px-4 py-3 flex justify-between">
+        <div className="font-serif text-white text-4xl">Fresh Market</div>
+
+        <nav className="flex text-lg items-center">
+          <form className="mr-4 rounded-full overflow-hidden">
+            <input
+              className="w-6 px-2 py-sm"
+              type="search"
+              placeholder="chercher un article"
+              aria-label="search"
+            />
+            <button
+              className="bg-secondary-600 px-2 py-sm text-white"
+              type="submit"
+            >
+              <i className="fas fa-search inline-block mr-2"></i>
+              <span>rechercher</span>
+            </button>
+          </form>
+
+          <button
+            className="fas fa-shopping-cart p-sm text-primary-600 text-2xl"
+            type="button"
+          ></button>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      
+      black: colors.black,
+      white: colors.white,
+
+      primary: colors.lime,
+      secondary: colors.teal,
+      tonic: colors.amber,
+
+      neutral: colors.warmGray,
+    },
+    spacing: {
+      0: "0",
+      xs: "0.125rem",
+      sm: "0.25rem",
+      1: "0.5rem",
+      2: "1rem",
+      3: "2rem",
+      4: "4rem",
+      5: "8rem",
+      6: "16rem",
+    },
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
       serif: ["Lobster", "cursive"],
