f01f60c5a447ae699929c4d8f37313b251c2c652
logic-fabric <lowic@logic-fabric.fr>
2021-07-29 15:22:40 +0200

    data: add some products data

---
+++ src/data/sample-data.js
@@ -0,0 +1,78 @@
+export const PRODUCTS = {
+  fruits: {
+    frenchName: "fruits",
+    items: [
+      {
+        name: "citron",
+        price: 0.99,
+        unit: "pièce",
+        image: "citron.png",
+      },
+      {
+        name: "citron vert",
+        price: 0.99,
+        unit: "pièce",
+        image: "citrons-verts.png",
+      },
+      {
+        name: "fraises",
+        price: 2.99,
+        unit: "kg",
+        image: "fraises.png",
+      },
+      {
+        name: "noix de coco",
+        price: 3.99,
+        unit: "pièce",
+        image: "noix-coco.png",
+      },
+      {
+        name: "clémentines",
+        price: 3.89,
+        unit: "kg",
+        image: "clementines.png",
+      },
+      {
+        name: "figues",
+        price: 5.99,
+        unit: "kg",
+        image: "figues.png",
+      },
+
+      {
+        name: "raisin blanc",
+        price: 4.49,
+        unit: "kg",
+        image: "raisin.png",
+      },
+      {
+        name: "kiwi",
+        price: 1.49,
+        unit: "pièce",
+        image: "kiwis.png",
+      },
+      {
+        name: "poires",
+        price: 3.49,
+        unit: "kg",
+        image: "poires.png",
+      },
+    ],
+  },
+  vegetables: {
+    frenchName: "légumes",
+    items: [],
+  },
+  freshProduces: {
+    frenchName: "produits frais",
+    items: [],
+  },
+  groceries: {
+    frenchName: "épicerie",
+    items: [],
+  },
+  drinks: {
+    frenchName: "boissons",
+    items: [],
+  },
+};
