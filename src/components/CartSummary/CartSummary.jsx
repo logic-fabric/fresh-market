4d38011b7eb91635394a47c92b9f5106665a8426
logic-fabric <lowic@logic-fabric.fr>
2021-08-28 10:38:48 +0200

    feature: static version of CartSummary component

--- src/components/CartSummary/CartSummary.jsx
+++ src/components/CartSummary/CartSummary.jsx
@@ -1,5 +1,33 @@
 export function CartSummary() {
   return (
-    <div className="h-6"></div>
+    <div>
+      <ul>
+        <li className="flex justify-between">
+          <span className="font-bold">articles</span>
+          <span>0,00&nbsp;€</span>
+        </li>
+
+        <li className="flex justify-between">
+          <span className="font-bold">livraison</span>
+          <span>0,00&nbsp;€</span>
+        </li>
+
+        <li className="my-2 text-center text-sm text-tonic-700">
+          j'ai un code promo&nbsp;!
+        </li>
+
+        <li className="flex justify-between my-3 text-xl">
+          <span className="font-bold">total</span>
+          <span>0,00&nbsp;€</span>
+        </li>
+      </ul>
+
+      <button
+        className="w-full m-auto px-3 py-1 my-2 rounded-3 text-white text-lg font-medium bg-gradient-to-r from-tonic-700 to-tonic-600 transition-all duration-300 hover:shadow-lg hover:opacity-90 hover:transform hover:-translate-y-px"
+        type="button"
+      >
+        passer commande
+      </button>
+    </div>
   );
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -6,7 +6,7 @@ import "./Cart.css";
 export function Cart() {
   return (
     <main className="content-wrapper flex">
-      <section className="w-3/4 ml-4 mr-3 my-3 p-3 rounded-1 bg-neutral-100">
+      <section className="w-3/4 ml-4 mr-3 my-3 px-3 py-2 rounded-1 bg-neutral-100">
         <h1 className="font-serif text-secondary-700 text-center text-3xl">
           mon panier
         </h1>
@@ -14,8 +14,8 @@ export function Cart() {
         <CartContent />
       </section>
 
-      <aside className="w-1/4 ml-3 mr-4 my-3 p-2 rounded-1 bg-primary-100">
-        <h2 className="text-primary-800 text-center text-xl font-bold">
+      <aside className="w-1/4 ml-3 mr-4 my-3 px-3 py-2 rounded-1 leading-7 bg-primary-100">
+        <h2 className="mb-3 font-serif text-primary-800 text-center text-3xl">
           récapitulatif
         </h2>
         <CartSummary />
