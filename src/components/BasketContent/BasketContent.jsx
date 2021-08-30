c824b02965a8937e4adfa4e4803b9b29e6af68a7
logic-fabric <lowic@logic-fabric.fr>
2021-08-30 23:00:31 +0200

    feature: BasketContentRow now functional

--- src/components/BasketContent/BasketContent.jsx
+++ src/components/BasketContent/BasketContent.jsx
@@ -1,3 +1,10 @@
+import { useDispatch } from "react-redux";
+
+import {
+  updateArticleQuantity,
+  removeArticleFromBasket,
+} from "../../redux/actions";
+
 export function BasketContent({ articles }) {
   if (articles.length === 0) {
     return (
@@ -34,6 +41,22 @@ function BasketContentRow({ article }) {
   const calculateSubTotalPrice = () =>
     (article.quantity * article.product.price).toFixed(2);
 
+  const decrementArticleQuantity = () => {
+    dispatch(
+      updateArticleQuantity(article.id, Math.max(article.quantity - 1, 0))
+    );
+  };
+
+  const incrementArticleQuantity = () => {
+    dispatch(updateArticleQuantity(article.id, article.quantity + 1));
+  };
+
+  const removeArticle = () => {
+    dispatch(removeArticleFromBasket(article.id));
+  };
+
+  const dispatch = useDispatch();
+
   return (
     <tr className="flex w-full mt-2 rounded-sm bg-white">
       <td className="flex items-center w-1/3 px-3">
@@ -60,6 +83,7 @@ function BasketContentRow({ article }) {
           <button
             className="fas fa-minus-square mx-2 text-secondary-700"
             type="button"
+            onClick={decrementArticleQuantity}
           ></button>
 
           <span className="inline-block w-3 text-center">
@@ -69,6 +93,7 @@ function BasketContentRow({ article }) {
           <button
             className="fas fa-plus-square mx-2 text-secondary-700"
             type="button"
+            onClick={incrementArticleQuantity}
           ></button>
         </div>
       </td>
@@ -83,6 +108,7 @@ function BasketContentRow({ article }) {
         <button
           className="far fa-times-circle text-danger-500 text-3xl"
           type="button"
+          onClick={removeArticle}
         ></button>
       </td>
     </tr>
