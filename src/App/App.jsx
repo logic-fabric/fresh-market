36dd517bf89959b3c434a6ed388f9b08d4010e21
logic-fabric <lowic@logic-fabric.fr>
2021-08-30 08:45:03 +0200

    refactor: remove App from index.jsx

---
+++ src/App/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Basket } from "../pages/Basket/Basket";
+import { Home } from "../pages/Home/Home";
+import { Header } from "../components/Header/Header";
+
+import { PRODUCTS } from "../data/sample-data";
+
+const PRODUCTS_CATEGORIES = Object.keys(PRODUCTS).map((category) => {
+  return {
+    en: category,
+    fr: PRODUCTS[category].frenchName,
+    icon: PRODUCTS[category].fontAwesomeIcon,
+  };
+});
+
+const productsCollections = [];
+
+for (let category of Object.keys(PRODUCTS)) {
+  productsCollections.push(PRODUCTS[category].items);
+}
+
+const PRODUCTS_LIST = productsCollections.flat();
+
+export function App() {
+  const [activeCategory, setActiveCategory] = useState("fruits");
+  const [isFilteringProducts, setIsFilteringProducts] = useState(false);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [basketCounter, setBasketCounter] = useState(0);
+
+  const filterProducts = (searchInputValue) => {
+    const filtered = PRODUCTS_LIST.filter((product) =>
+      product.name.toLowerCase().includes(searchInputValue.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
+  return (
+    <Router>
+      <Header
+        filterProducts={filterProducts}
+        setIsFilteringProducts={setIsFilteringProducts}
+        basketCounter={basketCounter}
+      />
+
+      <Switch>
+        <Route exact path="/">
+          <Home
+            products={PRODUCTS}
+            categories={PRODUCTS_CATEGORIES}
+            activeCategory={activeCategory}
+            setActiveCategory={setActiveCategory}
+            isFilteringProducts={isFilteringProducts}
+            filteredProducts={filteredProducts}
+            basketCounter={basketCounter}
+            setBasketCounter={setBasketCounter}
+          />
+        </Route>
+        <Route path="/mon-panier">
+          <Basket />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
