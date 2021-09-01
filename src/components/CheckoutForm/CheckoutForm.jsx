1c565f0732c4f261de1f5ccff7938f12eaf92aad
logic-fabric <lowic@logic-fabric.fr>
2021-09-01 20:44:17 +0200

    feature: add global validation for CheckoutForm

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -21,14 +21,40 @@ export function CheckoutForm() {
     return zipCodeRegex.test(zipCode);
   };
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [city, setCity] = useState("");
+
+  const [isFirstNameValid, setIsFirstNameValid] = useState(false);
+  const [isLastNameValid, setIsLastNameValid] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isAddressValid, setIsAddressValid] = useState(false);
+  const [isZipCodeValid, setIsZipCodeValid] = useState(false);
+  const [isCityValid, setIsCityValid] = useState(false);
+
   const [isFormValid, setIsFormValid] = useState(false);
 
-  const [isFirstNameValid, setIsFirstNameValid] = useState(true);
-  const [isLastNameValid, setIsLastNameValid] = useState(true);
-  const [isEmailValid, setIsEmailValid] = useState(true);
-  const [isAddressValid, setIsAddressValid] = useState(true);
-  const [isZipCodeValid, setIsZipCodeValid] = useState(true);
-  const [isCityValid, setIsCityValid] = useState(true);
+  useEffect(() => {
+    setIsFormValid(
+      isFirstNameValid &&
+        isLastNameValid &&
+        isEmailValid &&
+        isAddressValid &&
+        isZipCodeValid &&
+        isCityValid
+    );
+  }, [
+    isFirstNameValid,
+    isLastNameValid,
+    isEmailValid,
+    isAddressValid,
+    isZipCodeValid,
+    isCityValid,
+    isFormValid,
+  ]);
 
   return (
     <form className="text-primary-900">
@@ -39,21 +65,24 @@ export function CheckoutForm() {
           </label>
           <input
             className={`px-1 py-sm rounded-sm text-lg outline-none border-xs border-${
-              isFirstNameValid ? "primary-100" : "danger-300"
+              isFirstNameValid || !firstName ? "primary-100" : "danger-300"
             } focus:border-${
-              isFirstNameValid ? "primary-500" : "danger-300"
+              isFirstNameValid || !firstName ? "primary-500" : "danger-300"
             } transition-all duration-200`}
             id="first-name"
             type="text"
             name="first-name"
             placeholder="votre prénom"
+            defaultValue={firstName}
+            required
             onChange={() => {
               const firstNameInput = document.getElementById("first-name");
+              setFirstName(firstNameInput.value);
               setIsFirstNameValid(validateName(firstNameInput.value));
             }}
           />
           <p className="checkout-form__error px-1 text-danger-700 text-xs">
-            {isFirstNameValid ? "" : NAME_ERROR_MESSAGE}
+            {isFirstNameValid || !firstName ? "" : NAME_ERROR_MESSAGE}
           </p>
         </div>
 
@@ -63,21 +92,24 @@ export function CheckoutForm() {
           </label>
           <input
             className={`px-1 py-sm rounded-sm text-lg outline-none border-xs border-${
-              isLastNameValid ? "primary-100" : "danger-300"
+              isLastNameValid || !lastName ? "primary-100" : "danger-300"
             } focus:border-${
-              isLastNameValid ? "primary-500" : "danger-300"
+              isLastNameValid || !lastName ? "primary-500" : "danger-300"
             } transition-all duration-200`}
             id="last-name"
             type="text"
             name="last-name"
             placeholder="votre nom"
+            defaultValue={lastName}
+            required
             onChange={() => {
               const lastNameInput = document.getElementById("last-name");
+              setLastName(lastNameInput.value);
               setIsLastNameValid(validateName(lastNameInput.value));
             }}
           />
           <p className="checkout-form__error px-1 text-danger-700 text-xs">
-            {isLastNameValid ? "" : NAME_ERROR_MESSAGE}
+            {isLastNameValid || !lastName ? "" : NAME_ERROR_MESSAGE}
           </p>
         </div>
 
@@ -87,21 +119,24 @@ export function CheckoutForm() {
           </label>
           <input
             className={`px-1 py-sm rounded-sm text-lg outline-none border-xs border-${
-              isEmailValid ? "primary-100" : "danger-300"
+              isEmailValid || !email ? "primary-100" : "danger-300"
             } focus:border-${
-              isEmailValid ? "primary-500" : "danger-300"
+              isEmailValid || !email ? "primary-500" : "danger-300"
             } transition-all duration-200`}
             id="email"
             type="email"
             name="email"
             placeholder="votre adresse mail"
+            defaultValue={email}
+            required
             onChange={() => {
               const emailInput = document.getElementById("email");
+              setEmail(emailInput.value);
               setIsEmailValid(emailInput.validity.valid);
             }}
           />
           <p className="checkout-form__error px-1 text-danger-700 text-xs">
-            {isEmailValid ? "" : EMAIL_ERROR_MESSAGE}
+            {isEmailValid || !email ? "" : EMAIL_ERROR_MESSAGE}
           </p>
         </div>
       </fieldset>
@@ -113,21 +148,24 @@ export function CheckoutForm() {
           </label>
           <input
             className={`px-1 py-sm rounded-sm text-lg outline-none border-xs border-${
-              isAddressValid ? "primary-100" : "danger-300"
+              isAddressValid || !address ? "primary-100" : "danger-300"
             } focus:border-${
-              isAddressValid ? "primary-500" : "danger-300"
+              isAddressValid || !address ? "primary-500" : "danger-300"
             } transition-all duration-200`}
             id="address"
             type="text"
             name="address"
             placeholder="l'adresse de livraison de votre commande"
+            defaultValue={address}
+            required
             onChange={() => {
               const addressInput = document.getElementById("address");
+              setAddress(addressInput.value);
               setIsAddressValid(addressInput.value.length > 8);
             }}
           />
           <p className="checkout-form__error px-1 text-danger-700 text-xs">
-            {isAddressValid ? "" : ADDRESS_ERROR_MESSAGE}
+            {isAddressValid || !address ? "" : ADDRESS_ERROR_MESSAGE}
           </p>
         </div>
 
@@ -137,21 +175,24 @@ export function CheckoutForm() {
           </label>
           <input
             className={`px-1 py-sm rounded-sm text-lg outline-none border-xs border-${
-              isZipCodeValid ? "primary-100" : "danger-300"
+              isZipCodeValid || !zipCode ? "primary-100" : "danger-300"
             } focus:border-${
-              isZipCodeValid ? "primary-500" : "danger-300"
+              isZipCodeValid || !zipCode ? "primary-500" : "danger-300"
             } transition-all duration-200`}
             id="zip-code"
             type="text"
             name="zip-code"
             placeholder="24000"
+            defaultValue={zipCode}
+            required
             onChange={() => {
               const zipCodeInput = document.getElementById("zip-code");
+              setZipCode(zipCodeInput.value);
               setIsZipCodeValid(validateZipCode(zipCodeInput.value));
             }}
           />
           <p className="checkout-form__error px-1 text-danger-700 text-xs">
-            {isZipCodeValid ? "" : ZIP_CODE_ERROR_MESSAGE}
+            {isZipCodeValid || !zipCode ? "" : ZIP_CODE_ERROR_MESSAGE}
           </p>
         </div>
 
@@ -161,21 +202,24 @@ export function CheckoutForm() {
           </label>
           <input
             className={`px-1 py-sm rounded-sm text-lg outline-none border-xs border-${
-              isCityValid ? "primary-100" : "danger-300"
+              isCityValid || !city ? "primary-100" : "danger-300"
             } focus:border-${
-              isCityValid ? "primary-500" : "danger-300"
+              isCityValid || !city ? "primary-500" : "danger-300"
             } transition-all duration-200`}
             id="city"
             type="text"
             name="city"
             placeholder="Périgueux"
+            defaultValue={city}
+            required
             onChange={() => {
               const cityInput = document.getElementById("city");
+              setCity(cityInput.value);
               setIsCityValid(validateName(cityInput.value));
             }}
           />
           <p className="checkout-form__error px-1 text-danger-700 text-xs">
-            {isCityValid ? "" : NAME_ERROR_MESSAGE}
+            {isCityValid || !city ? "" : NAME_ERROR_MESSAGE}
           </p>
         </div>
       </fieldset>
@@ -186,7 +230,7 @@ export function CheckoutForm() {
             ? "block m-auto px-5 py-1 my-2 rounded-3 text-white text-center text-lg font-medium bg-gradient-to-r from-tonic-700 to-tonic-600 transition-all duration-300 hover:shadow-lg hover:opacity-90 hover:transform hover:-translate-y-px"
             : "block m-auto px-5 py-1 my-2 rounded-3 text-white text-center text-lg font-medium bg-neutral-400 cursor-default"
         }
-        disabled={isFormValid}
+        disabled={!isFormValid}
       >
         confirmer
       </button>
