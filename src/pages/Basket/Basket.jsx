01bdae6341291a9a114d78d7da6963af855f95d6
logic-fabric <lowic@logic-fabric.fr>
2021-10-02 21:41:38 +0200

    ui/ux: add an explicit button 'ajouter un autre article' on Basket Page

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { BasketContent } from "../../components/BasketContent/BasketContent";
@@ -16,13 +17,22 @@ export function Basket() {
         <BasketContent articles={articles} />
       </section>
 
-      <aside className="basket-summary-container w-1/4 ml-3 mr-4 my-3 px-3 py-2 rounded-1 leading-7 bg-primary-100">
-        <h2 className="mb-3 font-serif text-primary-800 text-center text-3xl">
-          récapitulatif
-        </h2>
+      <div className="w-1/4 ml-3 mr-4 my-3">
+        <Link
+          className="block relative m-auto px-3 py-1 mt-3 mb-4 rounded-3 text-white text-center text-lg font-medium bg-gradient-to-r from-tonic-700 to-tonic-600 transition-all duration-300 hover:shadow-lg hover:opacity-90 hover:transform hover:-translate-y-px"
+          to="/"
+        >
+          ajouter un autre article
+        </Link>
 
-        <BasketSummary articles={articles} />
-      </aside>
+        <aside className="basket-summary-container w-full px-3 py-2 rounded-1 leading-7 bg-primary-100">
+          <h2 className="mb-3 font-serif text-primary-800 text-center text-3xl">
+            récapitulatif
+          </h2>
+
+          <BasketSummary articles={articles} />
+        </aside>
+      </div>
     </main>
   );
 }
